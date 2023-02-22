5230598076acb7c0543079f75856384ead5c2a27
webdimension <office@webdimension.jp>
2023-02-22 19:40:09 +0900

    Fix

--- almalinux/ansible/playbook.sh
+++ almalinux/ansible/playbook.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-ansible-playbook -i hosts/docker site.yml -l almalinux8  -vvv
+#ansible-playbook -i hosts/docker site.yml -l almalinux8  -vvv
+ansible-playbook -i hosts/docker site.yml -l almalinux8 --tags mysql8 -vvv
+
